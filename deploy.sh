e0df9db01ec8661f5e6dc76a9fb9375f97614204
Gil <gilbertjvirgo@gmail.com>
2021-11-04 17:55:25 +0000

    Removed other git pulls

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
+git pull
+
 echo "Building frontend..."
 cd frontend
-git pull
 npm run build
 
 echo "Running the backend (in the background)..."
 cd ../backend
-git pull
 nohup node . &>./log &
 
 echo "Running webhook (in the background)..."
 cd ..
-git pull
 nohup node ./webhook &>./log &
