89e60d2f3042b0fa97cc0fc5854e5745c6c745f3
Gil <gilbertjvirgo@gmail.com>
2021-11-04 17:55:00 +0000

    Added git pull to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,15 @@
 
 echo "Building frontend..."
 cd frontend
+git pull
 npm run build
 
 echo "Running the backend (in the background)..."
 cd ../backend
+git pull
 nohup node . &>./log &
 
 echo "Running webhook (in the background)..."
 cd ..
+git pull
 nohup node ./webhook &>./log &
